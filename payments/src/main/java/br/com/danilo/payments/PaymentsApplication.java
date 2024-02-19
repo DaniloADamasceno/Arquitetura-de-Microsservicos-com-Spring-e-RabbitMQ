47aa7cc9ca950ae9901bb4448c53e4774b0bfb0f
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2024-02-19 11:32:12 -0300

    :bricks: configuração eureka server

--- payments/src/main/java/br/com/danilo/payments/PaymentsApplication.java
+++ payments/src/main/java/br/com/danilo/payments/PaymentsApplication.java
@@ -3,8 +3,10 @@ package br.com.danilo.payments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
